27bbd1611dcc7789016b640750bc7123894f2555
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-07-17 20:20:58 +0900

    feat: springBoot 기본 설정

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,30 @@
+package com.example.restfulwebservice;
+
+//lombok: 빈 클래스를 만들 떄 getter, setter, constructor 등을 자동 생성해줌
+//@Data를 추가해주면 됨
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor //디폴트 생성자를 같이 만드는 경우
+public class HelloWorldBean {
+
+    private String message;
+
+    //lombok을 사용하면 gettersetter 필요 x
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    public void setMessage(String message) {
+//        this.message = message;
+//    }
+
+    //@AllArgsConstructor를 사용하므로 constructor 사용 x
+//    public HelloWorldBean(String message) {
+//        this.message=message;
+//    }
+}
